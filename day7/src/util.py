1fd45c86fedc81aabcc85f0d3cff0efe46f44105
DavidW <david.wightman@gmail.com>
2024-12-07 12:43:29 +0100

    more refactoring day7

--- day7/src/util.py
+++ day7/src/util.py
@@ -4,22 +4,23 @@ def convertToInts(row):
     temp.remove('')
     row[1] = [int(x) for x in temp]
 
-def encodeOperators(ops, key, count, factor):
+def encodeOperators(key, count, factor):
+    ops = []
     for op in range(count):
         ops.append(key % factor)
         key = int(key/factor)
+    return ops
 
 def calculateSolution(solnValues, key, factor):
-    ops = []
-    encodeOperators(ops, key, len(solnValues)-1, factor)
+    ops = encodeOperators(key, len(solnValues)-1, factor)
     soln = solnValues[0]
-    for bit, op in enumerate(ops):
+    for i, op in enumerate(ops):
         if op == 0:
-            soln += solnValues[1+bit]
+            soln += solnValues[1+i]
         elif op == 1:
-            soln *= solnValues[1+bit]
+            soln *= solnValues[1+i]
         else:
-            soln = int(str(soln) + str(solnValues[1+bit]))
+            soln = int(str(soln) + str(solnValues[1+i]))
     return soln
 
 def getCalTotal(line,factor):

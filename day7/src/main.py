af7e3e42ccd65f2466e2995900b7a604fe9ed6c6
DavidW <david.wightman@gmail.com>
2024-12-07 11:46:54 +0100

    add day7 part2 solution

--- day7/src/main.py
+++ day7/src/main.py
@@ -19,29 +19,15 @@ for line in inputFile:
     rows.append(line.replace('\n','').split(':'))
 
 calTotal = 0
+part2 = 0
 
 for i, row in enumerate(rows):
     util.convertToInts(row)
-    nums = row[1]
-    max = len(nums) - 1
-    for ii in range(2**max):
-        s = f"Row {i} Max {max} Soln {ii}:"
-        soln = nums[0]
-        for bit in range(max):
-            x = (ii >> bit) & 1
-            if x == 0:
-                s += "add,"
-                soln += nums[1+bit]
-            else:
-                s +="multiply,"
-                soln *= nums[1+bit]
-        #print(s)
-        if soln == row[0]:
-            print("Soln found")
-            calTotal += row[0]
-            break
+    calTotal += util.get2sSolution(row)
+    part2 += util.get3sSolution(row)
 
-print(f"calibration Total:{calTotal}")
+print(f"calibration Total: {calTotal}")
+print(f"Part2 calibration Total: {part2}")
 
 
 
--- day7/src/util.py
+++ day7/src/util.py
@@ -9,4 +9,44 @@ def getOperatorSizes(row):
     return len(row[1])
 
 
+def get2sSolution(row):
+    nums = row[1]
+    max = len(nums) - 1
+    for ii in range(2**max):
+        soln = nums[0]
+        for bit in range(max):
+            x = (ii >> bit) & 1
+            if x == 0:
+                soln += nums[1+bit]
+            else:
+                soln *= nums[1+bit]
+        #print(s)
+        if soln == row[0]:
+            #print("Soln found")
+            return row[0]
 
+    return 0
+
+def get3sSolution(row):
+    max = len(row[1]) - 1
+    for i in range(3**max):
+        nums = row[1].copy()
+        ops = []
+        x = i
+        for bit in range(max):
+            ops.append(x % 3)
+            x = int(x/3)
+        
+        soln = nums[0]
+        for bit, op in enumerate(ops):
+            if op == 0:
+                soln += nums[1+bit]
+            elif op == 1:
+                soln *= nums[1+bit]
+            else:
+                soln = int(str(soln) + str(nums[1+bit]))
+        if soln == row[0]:
+            print(f"Found: {row[0]}, nums:{nums}")
+            return row[0]
+        
+    return 0

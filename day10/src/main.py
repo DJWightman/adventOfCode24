a583a8fe5e053944e6c8b0b0bc20b1e42fd11606
DavidW <david.wightman@gmail.com>
2024-12-11 10:15:20 +0100

    day 10 refactoring

--- day10/src/main.py
+++ day10/src/main.py
@@ -1,27 +1,30 @@
 import util
 EXAMPLE_DATA = 0
+TRAIL_HEAD = 0
+TRAIL_PEAK = 9
+
+def getScore(pos, trailPaths):
+    ret = {"part1": 0, "part2": 0 }
+    if pos not in trailPaths:
+        trailPaths.append(tuple(pos))
+        ret["part1"] = 1
+    ret["part2"] = 1
+    return ret
 
 def findPath(trails, pos, trailPaths):
+    if util.trailValue(trails, pos) == TRAIL_PEAK:
+        return getScore(pos, trailPaths)
+
     ret = {"part1": 0, "part2": 0 }
-
-    if util.trailValue(trails, pos) == 9:
-        if pos not in trailPaths:
-            trailPaths.append(tuple(pos))
-            ret["part1"] = 1
-        ret["part2"] = 1
-        return ret
-
     directions = ((1,0), (-1,0), (0,1), (0,-1))
     for offset in directions:
         newPos = tuple(map(lambda x, y: x + y, pos, offset))
-
-        if not util.posInRange(newPos, len(trails)):
-            continue
-
-        if  util.trailValue(trails, newPos) - util.trailValue(trails, pos) == 1:
+        if ( util.posInRange(newPos, len(trails)) and
+             util.trailValue(trails, newPos) - util.trailValue(trails, pos) == 1 ):
+            # Add return values of recursive findPath() 
+            # for Part 1 and Part 2 to the return dictionary
             for key, value in findPath(trails, newPos, trailPaths).items():
                 ret[key] += value
-
     return ret
 
 def main():
@@ -32,7 +35,7 @@ def main():
 
     trailheads = [ (x , y)
                     for y, row in enumerate(trails) 
-                        for x, val in enumerate(row) if val == 0
+                        for x, val in enumerate(row) if val == TRAIL_HEAD
                 ]
 
     scores = {"part1": 0, "part2": 0 }

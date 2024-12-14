7f71921dc15432086b9ee4b78f7e201c3ce24668
DavidW <david.wightman@gmail.com>
2024-12-14 12:13:13 +0100

    add solution 2 for day14

--- day14/src/main.py
+++ day14/src/main.py
@@ -26,14 +26,14 @@ inputFile = open(filePath, 'r')
 
 robots = []
 quadrants = [0,0,0,0]
-fg = [[0 for c in range(GRID_X)] for r in range(GRID_Y)]
+# fg = [[0 for c in range(GRID_X)] for r in range(GRID_Y)]
 for line in inputFile:
     p, v = line.replace('\n','').split(' ')
     p = tuple(map(int, p.replace("p=",'').split(",")))
     v = tuple(map(int, v.replace("v=", '').split(",")))
     f = tuple(map(lambda a, b, c: (a + (MOVES * b))%c, p, v, (GRID_X, GRID_Y)))
     robots.append((p, v, f))
-    fg[f[1]][f[0]] += 1
+    #fg[f[1]][f[0]] += 1
     if f[0] < MID_X:
         if f[1] < MID_Y:
             quadrants[0] += 1
@@ -45,10 +45,32 @@ for line in inputFile:
         elif f[1] > MID_Y:
             quadrants[3] += 1
 
-fg = [[str(p) if p > 0 else '.' for p in r ]for r in fg]
-#print(*robots, sep='\n')
-#print(*fg, sep='\n')
+#fg = [[str(p) if p > 0 else '.' for p in r ]for r in fg]
+
 sf = 1
 for q in quadrants:
     sf = sf * q
 print(f"Robots in quadrants: {quadrants}, safety factor: {sf}")
+
+c = ''
+counter = 0
+while c != 'q' and c != 'Q':
+
+    g = [[0 for c in range(GRID_X)] for r in range(GRID_Y)]
+
+    for robot in robots:
+        f = tuple(map(lambda a, b, c: (a + (counter * b))%c, robot[0], robot[1], (GRID_X, GRID_Y)))
+        g[f[1]][f[0]] += 1
+    g = [['X' if p > 0 else '.' for p in r ]for r in g]
+    g = [''.join(r) for r in g]
+
+    stop = False
+    for r in g:
+        if "XXXXXXXXXXXXXXXXX" in r:
+            stop = True
+    if stop:
+        print(*g, sep='\n')
+        print(f"Counter is: {counter}, continue?")
+        c = input()
+
+    counter += 1
